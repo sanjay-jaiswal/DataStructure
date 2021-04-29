927565f4c5ee97bc41cef1833917ea21def9e6d7
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-29 13:02:11 +0530

    [Sanju] ADD : This is Linked list program

---
+++ DataStructure/LinkedList/LinkedList.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class LinkedList
+    {
+        internal Node head; //new 
+
+        /// <summary>
+        /// Adding nodes from begining
+        /// </summary>
+        /// <param name="data"></param>
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Display the all nodes.
+        /// </summary>
+        internal void Display()
+        {
+            Console.WriteLine("=====Read from left to right =====>>>>\n");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; //temp=null
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Insert a node at particular position.
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (position-- != 0) //
+                {
+
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    temp = temp.next;//1000
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
+        }
+
+        /// <summary>
+        /// Delete a node from first position.
+        /// </summary>
+        /// <returns></returns>
+        internal Node RemoveFirstNode()
+        {
+            if (this.head == null)
+                return null;
+            this.head = this.head.next;
+            return this.head;
+        }
+
+        /// <summary>
+        /// Delete a node from last position.
+        /// </summary>
+        /// <returns></returns>
+        internal Node RemovaLastNode()
+        {
+            if (head == null)
+                return null;
+            if (head.next == null)
+                return null;
+            Node newNode = head;
+            while (newNode.next.next != null)
+            {
+                newNode = newNode.next;
+            }
+            newNode.next = null;
+            return head;
+        }
+
+        /// <summary>
+        /// search a particular node.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        internal Node Search(int value)
+        {
+            while (this.head != null)
+            {
+                if (this.head.data == value)
+                {
+                    return this.head;
+                }
+                this.head = this.head.next;
+            }
+            return null;
+        }
+    }
+}
---
+++ DataStructure/LinkedList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==============  LINKED LIST DATA STRUCTURE  =================");
+
+
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+
+            Console.WriteLine("\nINSERT AT PERTICULAR POSITION");
+            linkedList.InsertAtParticularPosition(2, 100);
+            linkedList.InsertAtParticularPosition(3, 300);
+            linkedList.InsertAtParticularPosition(4, 400);
+            linkedList.Display();
+
+            linkedList.RemoveFirstNode();
+            linkedList.Display();
+
+            linkedList.RemovaLastNode();
+            linkedList.Display();
+
+            linkedList.Search(30);
+            linkedList.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
